37d53035cd415e7c7ec2bc91e15d1c57eeb46dcf
Teuku Rizqy Ramadhan <kikidotid@gmail.com>
2025-02-21 19:51:14 +0700

    add excel

--- src/siswa/siswa.controller.ts
+++ src/siswa/siswa.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Post, Query, Body, Param, Res } from '@nestjs/common';
 import { SiswaService } from './siswa.service';
 import { Response } from 'express';
-import * as json2csv from 'json2csv';
+import * as XLSX from 'xlsx';
 import { Parser } from 'json2csv';
 
 @Controller('siswa')
@@ -77,6 +77,42 @@ export class SiswaController {
       res.send(csv);
     } catch (error) {
       console.error('Error saat ekspor SP:', error);
+      res.status(500).send('Terjadi kesalahan pada server.');
+    }
+  }
+
+  @Get('export-excel')
+  async exportExcel(@Query('filter') filter: string, @Query('date') date: string, @Res() res: Response) {
+    try {
+      const data = await this.siswaService.getExportSP(filter, date);
+
+      if (!data || data.length === 0) {
+        return res.status(404).send('Tidak ada data yang tersedia.');
+      }
+
+      const orderedData = data.map((item, index) => ({
+        no: index + 1,
+        nama: item.nama,
+        jenisPelanggaran: item.jenisPelanggaran,
+        keterangan: item.keterangan,
+        totalsp: item.spCount,
+        tanggal: item.tanggal
+      }));
+
+      // Membuat worksheet dari data
+      const worksheet = XLSX.utils.json_to_sheet(orderedData);
+      const workbook = XLSX.utils.book_new();
+      XLSX.utils.book_append_sheet(workbook, worksheet, 'Data SP');
+
+      // Mengatur header untuk file Excel
+      res.setHeader('Content-Disposition', 'attachment; filename=export_sp.xlsx');
+      res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+
+      // Mengirim file Excel
+      const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'buffer' });
+      res.send(excelBuffer);
+    } catch (error) {
+      console.error('Error saat ekspor SP ke Excel:', error);
       res.status(500).send('Terjadi kesalahan pada server.');
     }
   }
